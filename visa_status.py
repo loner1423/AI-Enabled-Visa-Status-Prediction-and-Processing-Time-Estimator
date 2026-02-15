777c92d275245987300d63a1d6f4e4b90e09928a
loner1423 <psprbunny2@gmail.com>
2026-02-15 23:35:55 +0530

    Update visa_status.py

--- visa_status.py
+++ visa_status.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd # used to store and manipulate the data in table form
 from datetime import datetime # helps to convert the date strings into the real date objects
 
-df = pd.read_csv("AIVISASTATUSDATASET.csv")
+df = pd.read_csv("VISASTATUSDATASET.csv")
 
 print("Original DataFrame with Missing Values:\n", df)
 
@@ -16,12 +16,12 @@ miss= df.isnull().sum()
 print("\nMissing values count:\n", miss)
 
 # mode()[0]-> returns the most frequ date in the colm
-# fillna()-> replaces the missing values inside the df
+# # fillna()-> replaces the missing values inside the df
 df["RECEIVED_DATE"].fillna(df["RECEIVED_DATE"].mode()[0], inplace=True)
 df["DECISION_DATE"].fillna(df["DECISION_DATE"].mode()[0], inplace=True)
 
 df["CASE_STATUS"].fillna("Unknown", inplace=True)
-df["EMPLOYER_CITY"].fillna("Unknown", inplace=True)
+df["EMPLOYER_STATE"].fillna("Unknown", inplace=True)
 
 df["RECEIVED_DATE"] = pd.to_datetime(df["RECEIVED_DATE"])
 df["DECISION_DATE"] = pd.to_datetime(df["DECISION_DATE"])
@@ -29,11 +29,18 @@ df["DECISION_DATE"] = pd.to_datetime(df["DECISION_DATE"])
 print(df)
 
 df["processing_days"] = (df["DECISION_DATE"] - df["RECEIVED_DATE"]).dt.days
+df["processing_days"].fillna(df["processing_days"].mean(),inplace=True)
 print("\nAfter calculating processing days:\n", df)
 
-df_encoded = pd.get_dummies(df, columns=[ "CASE_STATUS", "EMPLOYER_CITY"])
+df_encoded = pd.get_dummies(df, columns=["CASE_STATUS","VISA_CLASS","EMPLOYER_STATE"])
 print("\nEncoded DataFrame:\n", df_encoded)
+
+
 df.fillna(df.mode().iloc[0], inplace=True)
+
+miss= df.isnull().sum()
 print("\nMissing values count:\n", miss)
+df.to_csv("AIVISASTATUSDATASET.csv",index=False)
 # converts the text to numeric columns
 # one-hot encoding - as the ML model requires the numeric data
+
